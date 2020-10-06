987b5848a12dd3ca9efb9df78aa245b1c8aa4fe8
Lorenz Eigenstetter <lorenz.eigenstetter@edvschule-plattling.de>
2020-10-06 15:09:10 +0200

    add rooting

--- frontend/photostock/src/app/app.module.ts
+++ frontend/photostock/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
