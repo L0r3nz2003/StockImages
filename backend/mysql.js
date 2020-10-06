55c16da787b9cdc79231213b525bae46836cec8f
Jonathan Komma <j.komma@pfelling.de>
2020-10-06 19:00:37 +0200

    db server adresse geändert

--- backend/mysql.js
+++ backend/mysql.js
@@ -1,7 +1,7 @@
 mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "192.168.48.21",
+  host: "mysql.dvs-plattling.de",
   port: "3306",
   user: "leigenst",
   password: "leigenst",
