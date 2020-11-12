73ac22edb7b936ac6b14b9c60c13f6adc1299cf5
Lorenz Eigenstetter <lorenz.eigenstetter@edvschule-plattling.de>
2020-11-12 08:31:28 +0100

    Add method change Name

--- frontend/photostock/src/app/login/login.component.ts
+++ frontend/photostock/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,25 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+  login() {
+    var username: string = document.getElementById('inputusername').innerHTML;
+    var password: string = document.getElementById('inputPassword').innerHTML;
+
+    if(isNullOrUndefined(username) || username.length == 0) {
+      //TODO: output message;
+        return;
+    }
+
+    if(isNullOrUndefined(password) || password.length == 0) {
+      //TODO: output message;
+        return;
+    }
+
+
+  }
+
 
 }
