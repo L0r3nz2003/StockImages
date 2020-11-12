7ec27f03bc2e9f53adaf9c1862b9c3b2c2ccdfdc
Jonathan Komma <j.komma@pfelling.de>
2020-11-12 21:22:12 +0100

    anmelden begonnen

--- frontend/photostock/src/app/login/login.component.ts
+++ frontend/photostock/src/app/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../interfaces/user';
+import { UserService } from '../user.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  //users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
+
+  login(name: string):void {
+    alert(name);
+    /*if(!name){return;}
+    this.userService.getUserByName(name)
+    .subscribe(user => {
+      this.users.push(user)
+    })*/;
+
+  }
+
+
 
 }
