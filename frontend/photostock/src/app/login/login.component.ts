1acb4b3706ea2fb65a0930c01c91f4cc9e5fa423
Lorenz Eigenstetter <lorenz.eigenstetter@edvschule-plattling.de>
2020-11-17 11:55:31 +0100

    Update login component

--- frontend/photostock/src/app/login/login.component.ts
+++ frontend/photostock/src/app/login/login.component.ts
@@ -1,35 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { isNullOrUndefined } from 'util';
 
+import { User } from '../interfaces/user';
+import { UserService } from '../user.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  //users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
 
   }
 
-  login() {
-    var username: string = document.getElementById('inputusername').innerHTML;
-    var password: string = document.getElementById('inputPassword').innerHTML;
+  login(): void {
+    var username: HTMLElement = document.getElementById('inputusername');
+    var password: HTMLElement = document.getElementById('inputPassword');
 
-    if(isNullOrUndefined(username) || username.length == 0) {
-      //TODO: output message;
+    if(isNullOrUndefined(username.innerHTML.length) || username.innerHTML.length == 0) {
+        	username.style.borderColor = "red";
         return;
     }
 
-    if(isNullOrUndefined(password) || password.length == 0) {
-      //TODO: output message;
+    if(isNullOrUndefined(password.innerHTML.length) || password.innerHTML.length == 0) {
+          username.style.borderColor = "red";
         return;
     }
 
-
+    alert(name);
+    if(!name){return;}
+    this.userService.getUserByName(name)
+    .subscribe(user => {
+      this.userService.push(user)
+    });
   }
-
 
 }
