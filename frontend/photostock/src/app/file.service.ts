8c49e31e7d408c8d2755e44662249ed8216d273a
Lorenz <banana007.privat@gmail.com>
2021-02-23 12:42:05 +0100

    file service

---
+++ frontend/photostock/src/app/file.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {MessageService} from "./message.service";
+import {User} from "./interfaces/user";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FileService {
+
+  private url = 'http://localhost:3000/';
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) {
+  }
+
+
+  upload(file: FormData): void {
+    this.http.post<any>(this.url + "img/upload", file).subscribe(
+      data => console.log('success', data),
+      error => {
+        console.log(error);
+        alert(error.error.error);
+      }
+    );
+  }
+}
