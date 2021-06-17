bb75af89b6a05032909a1da17cabe31680e432ba
Jonathan Komma <jonatha.komma@edvschule-plattling.de>
2021-06-17 08:44:24 +0200

    increse geht, decrese ned

--- backend/server/src/database/mysql.js
+++ backend/server/src/database/mysql.js
@@ -12,4 +12,13 @@ connection.connect((error) => {
   if (error) throw error;
 });
 
+runQuery = async (str, replacements) => {
+  return new Promise((resolve) => {
+    db.query(str, replacements, (err, rows) => {
+      if (err) rejects(err.toString());
+      resolve(rows);
+    });
+  });
+};
+
 module.exports = connection;
